f1364c7d3b3209067460f9f8c3dd33baf5e2a9c7
Joe Gibbs <jgibbs-murray94@hotmailco.uk>
2017-01-27 12:37:45 +0000

    Ben 3

--- Bookstore1/src/main/java/Bookstore/Bookstore1/Orders.java
+++ Bookstore1/src/main/java/Bookstore/Bookstore1/Orders.java
@@ -1,5 +1,62 @@
 package Bookstore.Bookstore1;
 
 public class Orders {
+	
+	@Id
+	@GeneratedValue
+	protected Long id;
+	
+	@Column (length = 50)
+	protected String orderName;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOrderName() {
+		return orderName;
+	}
+
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+
+	public String getOrderDescription() {
+		return orderDescription;
+	}
+
+	public void setOrderDescription(String orderDescription) {
+		this.orderDescription = orderDescription;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Float getTotal() {
+		return total;
+	}
+
+	public void setTotal(Float total) {
+		this.total = total;
+	}
+
+	@Column (length = 50)
+	protected String orderDescription;
+	
+	@Column (length = 50)
+	protected int quantity;
+	
+	@Column (length = 50)
+	protected Float total;
+
 
 }
